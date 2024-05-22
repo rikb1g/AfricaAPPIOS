cb84d08d9a9ab390340c991c3514ec3a794c6bfe
Ricardo Sousa <r.sousa.888@gmail.com>
2024-05-22 15:11:52 +0100

    finished project

---
+++ Africa/AnimalGridItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Ricardo Sousa on 17/05/2024.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalGridItemView()
+}
---
+++ Africa/CenterModifier.swift
@@ -0,0 +1,8 @@
+//
+//  CenterModifier.swift
+//  Africa
+//
+//  Created by Ricardo Sousa on 22/05/2024.
+//
+
+import Foundation
